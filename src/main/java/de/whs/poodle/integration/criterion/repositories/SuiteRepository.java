9868b1b750bbb96eab32cbe1ec32ba294658d6bc
Unknown <marcel.kocks@hotmail.de>
2018-12-20 14:55:42 +0100

    imporved error logging

--- src/main/java/de/whs/poodle/integration/criterion/repositories/SuiteRepository.java
+++ src/main/java/de/whs/poodle/integration/criterion/repositories/SuiteRepository.java
@@ -1,19 +1,26 @@
 package de.whs.poodle.integration.criterion.repositories;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import de.whs.poodle.WorksheetAutoUnlockScheduler;
 import de.whs.poodle.integration.criterion.CriterionProperties;
 import de.whs.poodle.integration.criterion.beans.Suite;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.ConnectException;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 @Repository
 public class SuiteRepository {
     private final CriterionProperties criterion;
+
+    private Logger log = LoggerFactory.getLogger(SuiteRepository.class);
+
 
     private static ObjectMapper mapper = new ObjectMapper();
 
@@ -29,13 +36,19 @@ public class SuiteRepository {
             conn.setRequestProperty("Accept", "application/json");
 
             if (conn.getResponseCode() != 200) {
-                //TODO:log
+                if (conn.getResponseCode() != 404) {
+                    log.error("unexpected ResponseCode("
+                            + conn.getResponseCode()
+                            + ") while getting suite from criterion with id=" + id);
+                }
                 return null;
             }
 
             return mapper.readValue(new InputStreamReader(conn.getInputStream()), Suite.class);
+        } catch (ConnectException e) {
+            log.error(String.format("could not connect to criterion: %s/suite/%d", criterion.getBaseUrl(), id));
         } catch (IOException e) {
-            System.out.println("error getting suite from criterion id=" + id);
+            log.error(String.format("error getting suite from criterion id=%d", id), e);
         }
         return null;
     }
