3b52fdf00f4f101041ac8cdb2f4459d4ed07c9d0
Unknown <arthur-ott96@web.de>
2019-01-09 18:51:10 +0100

    change suiteResult bean

--- src/main/java/de/whs/poodle/integration/criterion/beans/SuiteResult.java
+++ src/main/java/de/whs/poodle/integration/criterion/beans/SuiteResult.java
@@ -8,30 +8,47 @@ public class SuiteResult {
     @JsonProperty("id")
     public int id;
 
+    @JsonProperty("suiteId")
+    public int suiteId;
+
+    @JsonProperty("userId")
+    public int userId;
+
     @JsonProperty("compileError")
     public String compileError;
 
-    @JsonProperty("Status")
+    @JsonProperty("status")
     public int status;
 
     @JsonProperty("creationTime")
     public String creationTime;
 
-    @JsonProperty("tests")
-    public TestResult[] tests;
+    @JsonProperty("successCount")
+    public int successCount;
+
+    @JsonProperty("testCount")
+    public int testCount;
+
+   // @JsonProperty("tests")
+   // public TestResult[] tests;
 
     @Override
     public String toString() {
         return "Suite{" +
                 "id=" + id +
                 ", creationTime='" + creationTime + '\'' +
-                ", tests=" + Arrays.toString(tests) +
+                ", successCount='" + successCount + '\'' +
+                ", testCount='" + testCount + '\'' +
+             //   ", tests=" + Arrays.toString(tests) +
                 '}';
     }
 
-    public void SetInfo(int id, String compErr, int status) {
+    public void SetInfo(int id, int suiteId, String compErr, int status, int successCount, int testCount) {
         this.id = id;
+        this.suiteId = suiteId;
         this.compileError = compErr;
         this.status = status;
+        this.successCount = successCount;
+        this.testCount = testCount;
     }
 }
