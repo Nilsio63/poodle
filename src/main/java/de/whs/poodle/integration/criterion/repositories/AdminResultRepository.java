042e8243fff3988132d291a0c28b26aa4989e3ec
Nils <nilsbalke@t-online.de>
2019-01-22 13:17:47 +0100

    implemented BaseRepository in SuiteResultRepository

--- src/main/java/de/whs/poodle/integration/criterion/repositories/AdminResultRepository.java
+++ src/main/java/de/whs/poodle/integration/criterion/repositories/AdminResultRepository.java
@@ -1,15 +1,9 @@
 package de.whs.poodle.integration.criterion.repositories;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.whs.poodle.integration.criterion.CriterionProperties;
 import de.whs.poodle.integration.criterion.beans.SuiteResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
-
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
 @Repository
 public class AdminResultRepository extends BaseRepository<SuiteResult[]> {
--- src/main/java/de/whs/poodle/integration/criterion/repositories/SuiteResultRepository.java
+++ src/main/java/de/whs/poodle/integration/criterion/repositories/SuiteResultRepository.java
@@ -1,41 +1,18 @@
 package de.whs.poodle.integration.criterion.repositories;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import de.whs.poodle.integration.criterion.CriterionProperties;
 import de.whs.poodle.integration.criterion.beans.SuiteResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-
 @Repository
-public class SuiteResultRepository {
-    private final CriterionProperties criterion;
-    private static ObjectMapper mapper = new ObjectMapper();
-
+public class SuiteResultRepository extends BaseRepository<SuiteResult[]> {
     @Autowired
     public SuiteResultRepository(CriterionProperties criterion) {
-        this.criterion = criterion;
+        super(criterion, SuiteResult[].class);
     }
 
     public SuiteResult[] get(int userId, String id) {
-        try {
-            URL url = new URL(String.format("%s/suite/%s/result?userId=%d", criterion.getBaseUrl(), id, userId));
-            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-            conn.setRequestProperty("Accept", "application/json");
-
-            if (conn.getResponseCode() != 200) {
-                //TODO:log
-                return null;
-            }
-            // change to bean suiteResult
-            return mapper.readValue(new InputStreamReader(conn.getInputStream()), SuiteResult[].class);
-        } catch (IOException e) {
-            System.out.println("error getting suiteresult from criterion id=" + id);
-        }
-        return null;
+        return get(String.format("suite/%s/result?userId=%d", id, userId));
     }
 }
